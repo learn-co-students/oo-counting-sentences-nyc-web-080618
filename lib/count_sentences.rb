fcaf40e4fa0c7deda3ca8ca4fd6902cbcd993748
Yixuan Wang <ywang0506@gmail.com>
2018-08-08 01:37:22 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+   if self.end_with?(".")
+     return true
+   else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+       return false
+     end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+       return false
+     end
   end
 
   def count_sentences
 
+    count = 0
+    self.split(" ").each do |w|
+      if w.end_with?("?") || w.end_with?(".") || w.end_with?("!")
+        count += 1
+      end
+    end
+    count
+    
   end
+
 end
